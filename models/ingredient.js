e1e6c619bdc44b74da6ee7b70bd7753ae74459cf
tshao <tshao0602@gmail.com>
2022-05-19 14:16:43 -0700

    phase 10 done

--- models/ingredient.js
+++ models/ingredient.js
@@ -3,7 +3,12 @@ module.exports = (sequelize, DataTypes) => {
   const Ingredient = sequelize.define('Ingredient', {
     amount: DataTypes.NUMERIC,
     measurementUnitId: DataTypes.INTEGER,
-    foodStuff: DataTypes.STRING,
+    foodStuff: {
+      type:DataTypes.STRING,
+      valide: {
+        notEmpty:true
+      },
+    },
     recipeId: DataTypes.INTEGER
   }, {});
   Ingredient.associate = function(models) {
